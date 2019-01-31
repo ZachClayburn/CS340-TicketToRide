69e66d586fa1b787add2d5299f4676476e4ea0ad
travisdb94 <46764866+travisdb94@users.noreply.github.com>
2019-01-30 19:36:13 -0700

    Added files
    Join game isn't finished, but the create game button works so Create game is ready. Also unsure if the parameters for game info class is right so let me know if I need to make changes.

---
+++ app/src/main/java/com/tickettoride/activities/GameInfo.java
@@ -0,0 +1,28 @@
+package com.tickettoride.activities;
+
+public class GameInfo { //used for when creating a new game
+    private String gameID = "";
+    private String groupName = "";
+    private int numPlayer = 0;
+
+    public void setGameID(String gameID) {
+        this.gameID = gameID;
+    }
+    public String getGameID() {
+        return gameID;
+    }
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+    public void setNumPlayer(int numPlayer) {
+        this.numPlayer = numPlayer;
+    }
+
+    public int getNumPlayer() {
+        return numPlayer;
+    }
+}
