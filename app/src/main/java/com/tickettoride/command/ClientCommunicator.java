f4e95843fd4cc9622d459392a79866b90d5b6050
Jacobus Crawford <cobi1kenobi4@gmail.com>
2019-02-02 15:16:24 -0700

    got most of the websocket stuff taken care of. just need to know  the callback methods of the buisness end on both the the server and client sides to finish it off

--- app/src/main/java/com/tickettoride/command/ClientCommunicator.java
+++ app/src/main/java/com/tickettoride/command/ClientCommunicator.java
@@ -1,86 +1,130 @@
 package com.tickettoride.command;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonIOException;
-import com.google.gson.JsonSyntaxException;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
+
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.drafts.Draft;
+import org.java_websocket.drafts.Draft_6455;
+import org.java_websocket.handshake.ServerHandshake;
+
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.io.OutputStreamWriter;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import command.Command;
 import command.Response;
 
 public class ClientCommunicator {
 
-    public static ClientCommunicator SINGLETON = new ClientCommunicator();
-
-    public ClientCommunicator() { }
-
-    public Response send(Command command) throws Throwable {
-        HttpURLConnection connection = openConnection(ServerCommunicator.COMMAND_DESIGNATOR);
-        sendToServerCommunicator(connection, command);
-        Response response = getResponse(connection);
-        return response;
-    }
-
+    private WebSocketClient mWebSocketClient;
     protected static Gson gson = new Gson();
 
-    protected HttpURLConnection openConnection(String contextIdentifier) {
-        HttpURLConnection result = null;
-        try {
-            URL url = new URL(URL_PREFIX + contextIdentifier);
-            result = (HttpURLConnection) url.openConnection();
-            result.setRequestMethod(HTTP_POST);
-            result.setDoOutput(true);
-            result.connect();
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    private static ClientCommunicator SINGLETON = new ClientCommunicator();
 
-        return result;
-    }
-
-    protected void sendToServerCommunicator(HttpURLConnection connection, Object objectToSend) {
+    private ClientCommunicator() {
         try {
-            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(connection.getOutputStream());
-            gson.toJson(objectToSend, outputStreamWriter);
-            outputStreamWriter.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+            //change the websockethost to the actual host IP (probably 10.0.0.2 if I remember right)
+            mWebSocketClient = new WebSocketClient(new URI("ws://websockethost:8080"), new Draft_6455()) {
 
-    protected Response getResponse(HttpURLConnection connection) throws Throwable {
-        Response result = null;
-        try {
-            if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
-                if (connection.getContentLength() == -1) {
-                    InputStreamReader inputStreamReader = new InputStreamReader(connection.getInputStream());
-                    result = gson.fromJson(inputStreamReader, Response.class);
-                    inputStreamReader.close();
+                @Override
+                public void onMessage(String message) {
+                    Response response=gson.fromJson(message, Response.class);
+                    //send it off to whatever callback method will handle the response
                 }
-            } else {
-                throw new Exception(String.format("http code %d", connection.getResponseCode()));
-            }
-        } catch (JsonSyntaxException | JsonIOException | IOException e) {
-            e.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
+
+                @Override
+                public void onOpen(ServerHandshake handshake) {
+
+                }
+
+                @Override
+                public void onClose(int code, String reason, boolean remote) {
+
+                }
+
+                @Override
+                public void onError(Exception ex) {
+
+                }
+            };
+        }catch(URISyntaxException e){
+            
         }
-        if (result.isThrowable()) {
-            try { throw result.getThrowable(); }
-            catch (ClassCastException e) { throw result.getThrowable(); }
-        }
-        return result;
     }
 
-    //Auxiliary Constants, Attributes, and Methods
-    private static final String SERVER_HOST = "localhost";
-    private static final String URL_PREFIX = "http://" + SERVER_HOST + ":" + ServerCommunicator.getServerPortNumber();
-    private static final String HTTP_POST = "POST";
+    public static ClientCommunicator getSINGLETON() {
+        return SINGLETON;
+    }
+    
+    public void send(Command command){
+        String message=gson.toJson(command);
+        mWebSocketClient.send(message);
+    }
+
+    //    public Response send(Command command) throws Throwable {
+//        HttpURLConnection connection = openConnection(ServerCommunicator.COMMAND_DESIGNATOR);
+//        sendToServerCommunicator(connection, command);
+//        Response response = getResponse(connection);
+//        return response;
+//    }
+//
+//    protected static Gson gson = new Gson();
+//
+//    protected HttpURLConnection openConnection(String contextIdentifier) {
+//        HttpURLConnection result = null;
+//        try {
+//            URL url = new URL(URL_PREFIX + contextIdentifier);
+//            result = (HttpURLConnection) url.openConnection();
+//            result.setRequestMethod(HTTP_POST);
+//            result.setDoOutput(true);
+//            result.connect();
+//        } catch (MalformedURLException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        return result;
+//    }
+//
+//    protected void sendToServerCommunicator(HttpURLConnection connection, Object objectToSend) {
+//        try {
+//            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(connection.getOutputStream());
+//            gson.toJson(objectToSend, outputStreamWriter);
+//            outputStreamWriter.close();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    protected Response getResponse(HttpURLConnection connection) throws Throwable {
+//        Response result = null;
+//        try {
+//            if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+//                if (connection.getContentLength() == -1) {
+//                    InputStreamReader inputStreamReader = new InputStreamReader(connection.getInputStream());
+//                    result = gson.fromJson(inputStreamReader, Response.class);
+//                    inputStreamReader.close();
+//                }
+//            } else {
+//                throw new Exception(String.format("http code %d", connection.getResponseCode()));
+//            }
+//        } catch (JsonSyntaxException | JsonIOException | IOException e) {
+//            e.printStackTrace();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        if (result.isThrowable()) {
+//            try { throw result.getThrowable(); }
+//            catch (ClassCastException e) { throw result.getThrowable(); }
+//        }
+//        return result;
+//    }
+//
+//    //Auxiliary Constants, Attributes, and Methods
+//    private static final String SERVER_HOST = "localhost";
+//    private static final String URL_PREFIX = "http://" + SERVER_HOST + ":" + ServerCommunicator.getServerPortNumber();
+//    private static final String HTTP_POST = "POST";
 }
