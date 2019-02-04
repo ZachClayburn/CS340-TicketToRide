c62c4218834242371d2acca62c75827f322a0d5c
Travis Bradshaw <travisdb94@gmail.com>
2019-02-04 11:34:59 -0800

    More files

--- app/src/main/java/com/tickettoride/activities/JoinGameActivity.java
+++ app/src/main/java/com/tickettoride/activities/JoinGameActivity.java
@@ -1,27 +1,32 @@
-package com.tickettoride.activities;
+package com.bignerdranch.android.tickettoride;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 
 public class JoinGameActivity extends AppCompatActivity {
-    private TextView gameName;
+    //private TextView gameName;
     private RecyclerView gameList;
     private Button createGame;
     private Adapter adapter;
-    //private ArrayList<>
+    private ArrayList<GameInfo> games;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.join_game);
-        gameName = (TextView) findViewById(R.id.game_list);
+        //gameName = (TextView) findViewById(R.id.game_list);
         gameList = (RecyclerView) findViewById(R.id.recycler_view);
         createGame = (Button) findViewById(R.id.create_game);
         createGame.setOnClickListener(new View.OnClickListener() {
@@ -31,40 +36,47 @@ public class JoinGameActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        //adapter = new Adapter(this, );
+        adapter = new Adapter(this, games);
+        gameList.setAdapter(adapter);
     }
 
     public class Adapter extends RecyclerView.Adapter<Holder> {
-
-        //public Adapter(Context context, "") {
-
-        //}
+        private ArrayList<GameInfo> listOfGames;
+        private LayoutInflater inflater;
+        public Adapter(Context context, ArrayList<GameInfo> listOfGames) {
+            this.listOfGames = listOfGames;
+            inflater = LayoutInflater.from(context);
+        }
         @Override
         public Holder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View view;
-            //return new Holder(view);
-            return null;
+            View view = inflater.inflate(R.layout.join_recycler_view, parent, false);
+            return new Holder(view);
         }
         @Override
         public void onBindViewHolder(Holder holder, int position) {
-
+            GameInfo game = listOfGames.get(position);
+            holder.bind(game);
         }
         @Override
         public int getItemCount() {
-            return 0;
+            return listOfGames.size();
         }
     }
     public class Holder extends RecyclerView.ViewHolder implements View.OnClickListener {
-
+        TextView gameName;
+        GameInfo game;
         public Holder(View view) {
            super(view);
+           gameName = (TextView) view.findViewById(R.id.game_list);
         }
-        public void bind() {
-
+        public void bind(GameInfo game) {
+            this.game = game;
+            gameName.setText(game.getGroupName());
         }
         @Override
         public void onClick(View view) {
-
+            Intent intent = new Intent(JoinGameActivity.this, LobbyActivity.class);
+            startActivity(intent);
         }
     }
 }
---
+++ app/src/main/java/com/tickettoride/activities/Player.java
@@ -0,0 +1,12 @@
+package com.bignerdranch.android.tickettoride;
+
+public class Player {
+    private String username;
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
