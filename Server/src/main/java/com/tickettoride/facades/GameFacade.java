5be1f905620652ea8b5d1eead5e0bc04075a68fe
Kristi Bresciano <kristi.aika29@gmail.com>
2019-02-09 14:32:26 -0700

    Updated game facade

--- Server/src/main/java/com/tickettoride/facades/GameFacade.java
+++ Server/src/main/java/com/tickettoride/facades/GameFacade.java
@@ -22,14 +22,15 @@ public class GameFacade extends BaseFacade{
     public void createGame(UUID connID, String groupName, int maxPlayers, String userID) throws Database.DatabaseException {
         String gameID = UUID.randomUUID().toString();
         Game game = new Game(gameID, groupName, 1, maxPlayers);
+        Player player = null;
         try (Database database = new Database()) {
             database.getGameDAO().addGame(game);
             User user = database.getUserDAO().getUser(userID);
-            Player player = new Player(user, game, UUID.randomUUID());
+            player = new Player(user, game, UUID.randomUUID());
             database.getPlayerDAO().addNewPlayer(player);
         }
         try {
-            Command command = new Command("GameController", "createGame");
+            Command command = new Command("GameController", "createGame", player, game);
             sendResponseToOne(connID, command);
         } catch (Throwable t) {
             Command command = new Command("GameController", "errorCreate", t);
@@ -39,6 +40,7 @@ public class GameFacade extends BaseFacade{
 
     public void joinGame(UUID connID, String userID, String gameID) throws Database.DatabaseException {
         Game game = null;
+        Player player = null;
         try (Database database = new Database()) {
             GameDAO dao = database.getGameDAO();
             game = dao.getGame(gameID);
@@ -47,11 +49,11 @@ public class GameFacade extends BaseFacade{
             dao.increasePlayerCount(gameID, game.getNumPlayer());
 
             User user = database.getUserDAO().getUser(userID);
-            Player player = new Player(user, game, UUID.randomUUID());
+            player = new Player(user, game, UUID.randomUUID());
             database.getPlayerDAO().addNewPlayer(player);
         }
         try {
-            Command command = new Command("GameController", "joinGame");
+            Command command = new Command("GameController", "joinGame", player, game);
             sendResponseToOne(connID, command);
             sendResponseToMainLobby(command);
         } catch (Throwable t) {
