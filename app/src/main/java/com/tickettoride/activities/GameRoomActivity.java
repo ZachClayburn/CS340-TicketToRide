2f3e0429657db78f9ffcbcc38756ab805d1f77c8
Travis Bradshaw <travisdb94@gmail.com>
2019-02-09 14:42:51 -0800

    FINAL

--- app/src/main/java/com/tickettoride/activities/GameRoomActivity.java
+++ app/src/main/java/com/tickettoride/activities/GameRoomActivity.java
@@ -6,7 +6,7 @@ import android.widget.Toast;
 
 import com.tickettoride.R;
 
-public class GameRoom extends AppCompatActivity {
+public class GameRoomActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_room);
