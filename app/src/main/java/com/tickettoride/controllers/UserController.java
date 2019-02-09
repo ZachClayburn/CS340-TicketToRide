7da26ee0685941f1f126045c7141c2a4cd1ebb4f
Unknown <jarekbird@gmail.com>
2019-02-09 14:59:25 -0700

    Singletons for Controllers

--- app/src/main/java/com/tickettoride/controllers/UserController.java
+++ app/src/main/java/com/tickettoride/controllers/UserController.java
@@ -1,4 +1,7 @@
 package com.tickettoride.controllers;
 
 public class UserController {
+    private static UserController SINGLETON = new UserController();
+    public static UserController getSingleton() { return SINGLETON; }
+    private UserController() {}
 }
