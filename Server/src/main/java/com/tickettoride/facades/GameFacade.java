c0cf1d7dea64c10b603767a3975b18bc177576b5
Zach Clayburn <zachclayburn@gmail.com>
2019-02-27 10:21:21 -0700

    Added the server command for accepting destination cards

--- Server/src/main/java/com/tickettoride/facades/GameFacade.java
+++ Server/src/main/java/com/tickettoride/facades/GameFacade.java
@@ -10,10 +10,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
-import java.util.UUID;
+import java.util.*;
 
 import command.Command;
 
@@ -111,6 +108,24 @@ public class GameFacade extends BaseFacade {
         sendResponseToRoom(connID, command);
     }
 
+    public void acceptDestinationCards(UUID connID, UUID sessionID, Player player,
+                                       Collection<DestinationCard> acceptedCards) {
+        try (var db = new Database()) {
+            //FIXME Come up with way to track that everyone has accepted their first cards
+            db.getDestinationCardDAO().acceptCards(player, acceptedCards);
+
+            var cmd = new Command(CONTROLLER_NAME, "setPlayerAcceptedCards",
+                    player, acceptedCards); //FIXME Make this command point to a method on the client
+
+            sendResponseToRoom(connID, cmd);
+
+            db.commit();
+
+        } catch (DatabaseException e) {
+            e.printStackTrace();//FIXME Add proper error handling
+        }
+    }
+
     void dealCards(UUID conID, UUID gameID) {
         logger.debug("Dealing to game " + gameID);
 
@@ -124,9 +139,11 @@ public class GameFacade extends BaseFacade {
 
             for (var player: db.getPlayerDAO().getGamePlayers(gameID)){
                 List<DestinationCard> offeredCards = new ArrayList<>();
-                for (int i = 0; i < 3; i++) {
-                    offeredCards.add(destinationDeck.remove());
-                }
+
+                offeredCards.add(destinationDeck.remove());
+                offeredCards.add(destinationDeck.remove());
+                offeredCards.add(destinationDeck.remove());
+
                 db.getDestinationCardDAO().offerCardsToPlayer(player, offeredCards);
                 commands.add(offerDestinationCards(player, offeredCards, 2));
             }
@@ -136,7 +153,7 @@ public class GameFacade extends BaseFacade {
             db.commit();
 
         } catch (DatabaseException e) {
-            e.printStackTrace();
+            e.printStackTrace();//FIXME Add proper error handling
         }
 
         commands.forEach(command -> sendResponseToRoom(conID, command));
@@ -146,13 +163,15 @@ public class GameFacade extends BaseFacade {
         return offerDestinationCards(player, offeredCards, 1);
     }
 
-    Command offerDestinationCards(Player player, List<DestinationCard> offeredCard,
+    Command offerDestinationCards(Player player, List<DestinationCard> offeredCards,
                                   int requiredToKeep) {
-        return null; //FIXME Make this command
+        return new Command(CONTROLLER_NAME, "offerDestinationCards",
+                player, offeredCards, requiredToKeep); //FIXME Make this command point to a method on the client
     }
 
     Command sendDestinationDeck(Queue<DestinationCard> deck) {
-        return null; //FIXME Make this command
+        return new Command(CONTROLLER_NAME, "updateDestinationDeck",
+                deck.size()); //FIXME Make this command point to a method on the client
     }
 
     private Player isAlreadyPlayer(User user, List<Player> players){
