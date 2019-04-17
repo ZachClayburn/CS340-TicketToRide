c40c6bb4be693f1ab3a0db410b7de08141bd3e5c
Unknown <jarekbird@gmail.com>
2019-04-17 09:26:16 -0600

    Mongo Reflection Fix

--- Mongo/src/main/java/com/tickettoride/database/MongoCommand.java
+++ Mongo/src/main/java/com/tickettoride/database/MongoCommand.java
@@ -6,27 +6,30 @@ import org.bson.Document;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MongoCommand {
     private MongoCollection collection;
     private String methodName;
-    private List<Class> parameterClasses;
+    private Class[] parameterClasses;
     private List<Object> parameters;
 
     public MongoCommand(MongoCollection collection, String methodName, List<Object> parameters) {
         this.collection = collection;
         this.methodName = methodName;
         this.parameters = parameters;
-        parameterClasses = new ArrayList<>();
-        for (Object parameter: parameters) {
-            parameterClasses.add(parameter.getClass());
+        parameterClasses = new Class[parameters.size()];
+        for (int x = 0; x < parameters.size(); x++) {
+            Object parameter = parameters.get(x);
+            parameterClasses[x] = parameter.getClass();
         }
     }
 
     public void execute() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
-        Method method = collection.getClass().getMethod(methodName, (Class[]) parameterClasses.toArray());
+        if (methodName == Database.INSERT_METHOD_NAME) { collection.insertOne(parameters.get(0)); return; }
+        Method method = collection.getClass().getMethod(methodName, parameterClasses);
         method.invoke(collection, parameters.toArray());
     }
 }
